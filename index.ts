fe67fff7f89058a9e730e3983bcec6935f6c3ff1
Kristjan Rajaniemi <Kristjan.rajaniemi@live.fi>
2022-09-02 11:38:13 +0300

    Index.ts tsconfig.json change

--- index.ts
+++ index.ts
@@ -4,38 +4,31 @@ dotenv.config();
 
 async function main() {
 	console.log('Trying to tweet');
-	if (
-		typeof process.env.BEARER_TOKEN === 'string' &&
-		typeof process.env.ACCESS_TOKEN === 'string' &&
-		typeof process.env.ACCESS_TOKEN_SECRET === 'string' &&
-		typeof process.env.CONSUMER_KEY === 'string' &&
-		typeof process.env.CONSUMER_SECRET === 'string'
-	) {
-		const access = process.env.ACCESS_TOKEN;
-		const accessSecret = process.env.ACCESS_TOKEN_SECRET;
-		const consumer = process.env.CONSUMER_KEY;
-		const consumerSecret = process.env.CONSUMER_SECRET;
-		const client = new TwitterApi({
-			appKey: consumer,
-			appSecret: consumerSecret,
-			accessToken: access,
-			accessSecret: accessSecret,
-		});
-		console.log('pasalusta');
 
-		// First, post all your images to Twitter
-		const mediaIds = await Promise.all([
-			// file path
-			client.v1.uploadMedia('./perstai.jpg'),
-		]);
-		console.log('Posting');
+	const access = process.env.ACCESS_TOKEN;
+	const accessSecret = process.env.ACCESS_TOKEN_SECRET;
+	const consumer = process.env.CONSUMER_KEY;
+	const consumerSecret = process.env.CONSUMER_SECRET;
+	const client = new TwitterApi({
+		appKey: consumer,
+		appSecret: consumerSecret,
+		accessToken: access,
+		accessSecret: accessSecret,
+	});
+	console.log('pasalusta');
 
-		// mediaIds is a string[], can be given to .tweet
-		await client.v1.tweet('Perstai!', {
-			media_ids: mediaIds,
-		});
-		console.log('Posted');
-	} else throw new Error(`Invalid tokens`);
+	// First, post all your images to Twitter
+	const mediaIds = await Promise.all([
+		// file path
+		client.v1.uploadMedia('./perstai.jpg'),
+	]);
+	console.log('Posting');
+
+	// mediaIds is a string[], can be given to .tweet
+	await client.v1.tweet('Perstai!', {
+		media_ids: mediaIds,
+	});
+	console.log('Posted');
 }
 
 const checkFriday = () => {
@@ -48,6 +41,6 @@ const checkFriday = () => {
 		void main();
 	}
 };
-
+console.log('Deployed');
 checkFriday();
 setInterval(checkFriday, 3600000); // one hour check.
